a693344b9ae74fded35320926196581e23b05265
Geonwoo Shin <dagarn@unopnd.com>
2023-02-16 18:30:17 +0900

    Make simple express server

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+const db = require("node-localdb");
+const People = db("people.json");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+  console.log("Listening on port 3000!");
 });
 
-app.get('/', (req, res) => {
-  const {param1} = req.query;
+app.get("/", (req, res) => {
+  const { param1 } = req.query;
 
-  res.send('Hello World!<br>Param1 = ' + param1);
+  res.send("Hello World!<br>Param1 = " + param1);
 });
 
 let nexPersonId = 3;
 const people = [
-  {id: 1, name: 'John', surname: 'Doe'},
-  {id: 2, name: 'Anna', surname: 'Dopey'},
+  { id: 1, name: "John", surname: "Doe" },
+  { id: 2, name: "Anna", surname: "Dopey" },
 ];
 
-app.get('/people', (req, res) => {
-  res.send(people);
+app.get("/people", (req, res) => {
+  People.find({}).then(function (pp) {
+    res.send(pp);
+  });
 });
 
-app.get('/people/:id', (req, res) => {
+app.get("/people/:id", (req, res) => {
   const personId = +req.params.id;
 
-  const person = people.find(person => person.id === personId);
+  const person = people.find((person) => person.id === personId);
 
-  if(!person) {
+  if (!person) {
     res.sendStatus(404);
     return;
   }
@@ -38,28 +43,32 @@ app.get('/people/:id', (req, res) => {
   res.send(person);
 });
 
-app.post('/people', (req, res) => {
-  if(!req.body){
-    res.status(400).json({ error: 'Body not specified' });
-    return;
-  }
+app.post("/people", (req, res) => {
+  // if (!req.body) {
+  //   res.status(400).json({ error: "Body not specified" });
+  //   return;
+  // }
 
-  if(!req.body.name){
-    res.status(400).json({ error: 'No name specified' });
-    return;
-  }
+  // if (!req.body.name) {
+  //   res.status(400).json({ error: "No name specified" });
+  //   return;
+  // }
 
-  if(!req.body.surname){
-    res.status(400).json({ error: 'No surname specified' });
-    return;
-  }
+  // if (!req.body.surname) {
+  //   res.status(400).json({ error: "No surname specified" });
+  //   return;
+  // }
 
-  const newPerson = {
-    ...req.body,
-    id: nexPersonId++
-  };
+  // const newPerson = {
+  //   ...req.body,
+  //   id: nexPersonId++,
+  // };
 
-  people.push(newPerson);
+  People.insert({ username: "jf", password: "123", email: "123@qq.com" }).then(
+    function (u) {
+      console.log(u); // print user, with a auto generate uuid
+    }
+  );
 
-  res.send(newPerson);
+  res.send({ username: "jf", password: "123", email: "123@qq.com" });
 });
